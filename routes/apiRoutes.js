f5056c035625eba2a1910e2263db2fe39f915e60
Money Penny <pennyquirino@gmail.com>
2021-03-16 10:22:28 -0400

    added code for html and api route files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,3 +11,44 @@ router.post("/api/workouts", (req, res) => {
         res.status(400).json(err);
     });
 });
+
+
+router.put("/api/workouts/:id", (req, res) => {
+    console.log(req.body);
+    Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        { $push: { exercises: req.body }},
+        { new: true }
+
+    ).then((dbWorkout) => res.json(dbWorkout));
+});
+
+router.get("api/workouts", (req, res) => {
+    Workout.find({})
+    .sort({date: -1})
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate ([
+        {
+            $addFields: {
+                totaleDuration: { "$sum: $exercises.duration"},
+            },
+        },
+    ]).sort({ date: -1})
+    .limit(7)
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    });
+});
+
+module.exports = router;
